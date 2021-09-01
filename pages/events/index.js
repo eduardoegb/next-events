5f2c4adf23fba2fe93d00e8a4ccf8938721169d3
Eduardo Gonzalez <eduardoegb@gmail.com>
2021-09-01 18:49:44 -0400

    Adding the Event List and its Items

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,5 +1,15 @@
+import EventList from '../../components/events/EventList';
+import { getFeaturedEvents } from '../../data/dummy-data';
+
 function EventsPage() {
-  return <div>Events Page</div>;
+  const items = getFeaturedEvents();
+
+  return (
+    <div>
+      Events Page
+      <EventList items={items} />
+    </div>
+  );
 }
 
 export default EventsPage;
